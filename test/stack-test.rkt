2a4eef27b926880952d6fd20b2a347187b143e34
Lehi Toskin <lehi@tosk.in>
2015-05-24 19:10:21 -0700

    Add more procedures and tests

--- test/stack-test.rkt
+++ test/stack-test.rkt
@@ -16,6 +16,7 @@
 (check-true (stack=? (push* (make-stack) 1 2 3)
                      (push (push (push (make-stack) 3) 2) 1)))
 
+; stack->list
 (let ([stk (make-stack)])
   (push! stk 'a)
   (check-false (stack-empty? stk))
@@ -24,6 +25,7 @@
   (pop! stk)
   (check-equal? (stack->list stk) '()))
 
+; stack=?
 (let ([stk123 (make-stack)]
       [stk*123 (make-stack)])
   (push*! stk*123 1 2 3)
@@ -31,3 +33,104 @@
   (push! stk123 2)
   (push! stk123 1)
   (check-true (stack=? stk*123 stk123)))
+
+; stack->string
+(check-false (string=? (stack->string (stack '(1 2 3 4))) "1234"))
+(check-false (string=? (stack->string (stack '(1 2 3 4))) "4321"))
+(check-true (string=? (stack->string (stack '(1 2 3 4))) "(4 3 2 1)"))
+(check-false (string=? (stack->string (make-stack)) ""))
+(check-true (string=? (stack->string (make-stack)) "()"))
+
+; push-dup
+(let ([stk1 (stack '(1 2 3))]
+      [stk2 (stack '(1 2 3))])
+  (push-dup! stk2)
+  (check-true (stack=? (push-dup stk1) stk2))
+  (check-true (stack=? (push-dup (make-stack)) (stack '(())))))
+
+; pop-all!
+(let ([stk (stack '(1 2 3))])
+  (pop-all! stk)
+  (check-true (stack-empty? stk)))
+
+; swap
+(check-exn exn:fail:contract? (λ () (swap (make-stack))))
+(check-exn exn:fail:contract? (λ () (swap! (make-stack))))
+(let ([stk1 (stack '(1))]
+      [stk2 (stack '(1 2 3))]
+      [stk3 (stack '(1 2 3))])
+  (swap! stk3)
+  (check-exn exn:fail:contract? (λ () (swap stk1)))
+  (check-true (stack=? (swap stk2) stk3)))
+
+; push-over
+(check-exn exn:fail:contract? (λ () (push-over (make-stack))))
+(check-exn exn:fail:contract? (λ () (push-over! (make-stack))))
+(let ([stk1 (stack '(1 2 3))]
+      [stk2 (stack '(1 2 3))])
+  (push-over! stk2)
+  (check-true (stack=? (push-over stk1) stk2)))
+
+; rotate
+(check-exn exn:fail:contract? (λ () (rotate (make-stack))))
+(check-exn exn:fail:contract? (λ () (rotate! (make-stack))))
+(let ([stk1 (stack '(3 2 1))]
+      [stk2 (stack '(3 2 1))])
+  (rotate! stk2)
+  (check-true (stack=? (rotate stk1) stk2))
+  (check-true (stack=? (rotate stk1) (stack '(1 3 2)))))
+
+; reverse-rotate
+; same as rotate twice
+(check-exn exn:fail:contract? (λ () (reverse-rotate (make-stack))))
+(check-exn exn:fail:contract? (λ () (reverse-rotate! (make-stack))))
+(let ([stk1 (stack '(1 2 3 4 5 6))]
+      [stk2 (stack '(1 2 3 4 5 6))]
+      [stk3 (stack '(1 2 3 4 5 6))]
+      [stk4 (stack '(1 2 3 4 5 6))])
+  (reverse-rotate! stk2)
+  (check-true (stack=? (reverse-rotate stk1) stk2))
+  (check-true (stack=? (reverse-rotate stk1) (rotate (rotate stk3))))
+  (rotate! stk4)
+  (rotate! stk4)
+  (check-true (stack=? stk2 stk4)))
+
+; pop-nip
+(check-exn exn:fail:contract? (λ () (pop-nip (make-stack))))
+(check-exn exn:fail:contract? (λ () (pop-nip! (make-stack))))
+(let ([stk1 (stack '(1 2 3))]
+      [stk2 (stack '(1 2 3))])
+  (pop-nip! stk2)
+  (check-true (stack=? (pop-nip stk1) stk2)))
+
+; push-tuck
+(check-exn exn:fail:contract? (λ () (push-tuck (make-stack))))
+(check-exn exn:fail:contract? (λ () (push-tuck! (make-stack))))
+(let ([stk1 (stack '(1 2 3))]
+      [stk2 (stack '(1 2 3))])
+  (push-tuck! stk2)
+  (check-true (stack=? (push-tuck stk1) stk2)))
+
+; push-pick
+(check-exn exn:fail:contract? (λ () (push-pick (make-stack) 0)))
+(check-exn exn:fail:contract? (λ () (push-pick (make-stack) 2)))
+(check-exn exn:fail:contract? (λ () (push-pick! (make-stack) 0)))
+(check-exn exn:fail:contract? (λ () (push-pick! (make-stack) 2)))
+(let ([stk1 (stack '(1 2 3))]
+      [stk2 (stack '(1 2 3))])
+  (push-pick! stk2 1)
+  (check-true (stack=? (push-pick stk1 1) stk2)))
+
+; roll
+; used in rotate and reverse-rotate
+(check-exn exn:fail:contract? (λ () (roll (make-stack) 0)))
+(check-exn exn:fail:contract? (λ () (roll (make-stack) 2)))
+(check-exn exn:fail:contract? (λ () (roll! (make-stack) 0)))
+(check-exn exn:fail:contract? (λ () (roll! (make-stack) 2)))
+(let ([stk1 (stack '(1 2 3))]
+      [stk2 (stack '(1 2 3))])
+  (roll! stk2 2)
+  (check-true (stack=? (roll stk1 2) stk2))
+  (check-true (stack=? (roll stk1 2) (rotate stk1)))
+  (check-true (stack=? (rotate stk1) stk2))
+  (check-true (stack=? (reverse-rotate stk1) (rotate stk2))))
--- typed-stack.rkt
+++ typed-stack.rkt
@@ -12,8 +12,14 @@
   (define (make-stack) : stack
     (stack empty))
   
+  ; creates a list from the stack as-is
   (define (stack->list [stk : stack]) : (Listof Any)
     (stack-contents stk))
+  
+  ; builds a string representation of the stack
+  ; with ordering from bottom to top
+  (define (stack->string [stk : stack]) : String
+    (with-output-to-string (λ () (printf "~a" (reverse (stack->list stk))))))
   
   (define (stack-empty? [stk : stack]) : Boolean
     (empty? (stack->list stk)))
@@ -65,4 +71,161 @@
   
   (define (push-dup! [stk : stack]) : Void
     (push! stk (top stk)))
+  
+  ; removes all items from the stack
+  (define (pop-all! [stk : stack]) : Void
+    (unless (stack-empty? stk)
+      (set-stack-contents! stk empty)))
+  
+  ; swaps the location of the two topmost items
+  (define (swap [stk : stack]) : stack
+    (cond [(< (stack-length stk) 2)
+           (raise-argument-error 'swap "stack-length >= 2" (stack-length stk))]
+          [else
+           (define one (top stk))
+           (define two (top (pop stk)))
+           (push (push (pop (pop stk)) one) two)]))
+  
+  (define (swap! [stk : stack]) : Void
+    (cond [(< (stack-length stk) 2)
+           (raise-argument-error 'swap! "stack-length >= 2" (stack-length stk))]
+          [else
+           (define one (top stk))
+           (define two (top (pop stk)))
+           (pop! stk)
+           (pop! stk)
+           (push! stk one)
+           (push! stk two)]))
+  
+  ; push a copy of the second topmost item onto the stack
+  (define (push-over [stk : stack]) : stack
+    (cond [(< (stack-length stk) 2)
+           (raise-argument-error 'push-over "stack-length >= 2"
+                                 (stack-length stk))]
+          [else
+           (define two (top (pop stk)))
+           (push stk two)]))
+  
+  (define (push-over! [stk : stack]) : Void
+    (cond [(< (stack-length stk) 2)
+           (raise-argument-error 'push-over! "stack-length >= 2"
+                                 (stack-length stk))]
+          [else
+           (define two (top (pop stk)))
+           (push! stk two)]))
+  
+  ; rotates the top three items downward
+  ; (stack '(3 2 1)) -> (stack '(1 3 2))
+  (define (rotate [stk : stack]) : stack
+    (cond [(< (stack-length stk) 3)
+           (raise-argument-error 'rotate "stack-length >= 3" (stack-length stk))]
+          [else (roll stk 2)]))
+  
+  (define (rotate! [stk : stack]) : Void
+    (cond [(< (stack-length stk) 3)
+           (raise-argument-error 'rotate! "stack-length >= 3" (stack-length stk))]
+          [else (roll! stk 2)]))
+  
+  ; rotates the top three items upward
+  ; (stack '(3 2 1)) -> (stack '(2 1 3))
+  ; equivalent to rotating twice
+  (define (reverse-rotate [stk : stack]) : stack
+    (cond [(< (stack-length stk) 3)
+           (raise-argument-error 'reverse-rotate "stack-length >= 3"
+                                 (stack-length stk))]
+          [else
+           #|(define one (top stk))
+           (define two (top (pop stk)))
+           (define three (top (pop (pop stk))))
+           (push (push (push (pop (pop (pop stk))) one) three) two)|#
+           (roll (roll stk 2) 2)]))
+  
+  (define (reverse-rotate! [stk : stack]) : Void
+    (cond [(< (stack-length stk) 3)
+           (raise-argument-error 'reverse-rotate! "stack-length >= 3"
+                                 (stack-length stk))]
+          [else
+           #|(define one (top stk))
+           (define two (top (pop stk)))
+           (define three (top (pop (pop stk))))
+           (pop! stk)
+           (pop! stk)
+           (pop! stk)
+           (push! stk one)
+           (push! stk three)
+           (push! stk two)|#
+           (roll! stk 2)
+           (roll! stk 2)]))
+  
+  ; removes the second topmost item from the stack
+  (define (pop-nip [stk : stack]) : stack
+    (cond [(< (stack-length stk) 2)
+           (raise-argument-error 'pop-nip "stack-length >= 2" (stack-length stk))]
+          [else
+           (define val (top stk))
+           (push (pop (pop stk)) val)]))
+  
+  (define (pop-nip! [stk : stack]) : Void
+    (cond [(< (stack-length stk) 2)
+           (raise-argument-error 'pop-nip! "stack-length >= 2"
+                                 (stack-length stk))]
+          [else
+           (define val (top stk))
+           (pop! stk)
+           (pop! stk)
+           (push! stk val)]))
+  
+  ; swaps the top two items and then pushes a copy of the former top item
+  (define (push-tuck [stk : stack]) : stack
+    (cond [(< (stack-length stk) 2)
+           (raise-argument-error 'push-tuck "stack-length >= 2"
+                                 (stack-length stk))]
+          [else
+           (push-over (swap stk))]))
+  
+  (define (push-tuck! [stk : stack]) : Void
+    (cond [(< (stack-length stk) 2)
+           (raise-argument-error 'push-tuck! "stack-length >= 2"
+                                 (stack-length stk))]
+          [else
+           (swap! stk)
+           (push-over! stk)]))
+  
+  ; pushes a copy of the specified index
+  (define (push-pick [stk : stack] [i : Index]) : stack
+    (define stk-len (stack-length stk))
+    (cond [(>= i stk-len)
+           (raise-argument-error 'push-pick! (format "i < ~a" stk-len) i)]
+          [else
+           (define val (list-ref (stack->list stk) i))
+           (push stk val)]))
+  
+  (define (push-pick! [stk : stack] [i : Index]) : Void
+    (define stk-len (stack-length stk))
+    (cond [(>= i stk-len )
+           (raise-argument-error 'push-pick! (format "i < ~a" stk-len) i)]
+          [else
+           (define val (list-ref (stack->list stk) i))
+           (push! stk val)]))
+  
+  ; removes the item at the index and pushes to the top of the stack
+  (define (roll [stk : stack] [i : Index]) : stack
+    (define stk-len (stack-length stk))
+    (cond [(>= i stk-len)
+           (raise-argument-error 'roll (format "i < ~a" stk-len) i)]
+          [else
+           (define lst (stack->list stk))
+           (define val (list-ref lst i))
+           (define-values (a b) (split-at lst i))
+           (push (stack (append a (cdr b))) val)]))
+  
+  (define (roll! [stk : stack] [i : Index]) : Void
+    (define stk-len (stack-length stk))
+    (cond [(>= i stk-len)
+           (raise-argument-error 'roll! (format "i < ~a" stk-len) i)]
+          [else
+           (define lst (stack->list stk))
+           (define val (list-ref lst i))
+           (define-values (a b) (split-at lst i))
+           (set-stack-contents! stk (cons val (append a (rest b))))]))
   )
